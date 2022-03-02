c7e10a59abaf77a1d519c45bd560df1787038704
Jcinc2 <91334198+Jcinc2@users.noreply.github.com>
2022-03-02 11:18:38 -0500

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -2,7 +2,7 @@ let handler = async (m, { text }) => {
   let user = global.DATABASE.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`*[ ⚠️ ] El usuario ${conn.getName(m.sender)} estará inactivo (AFK) no lo etiqueten*\n\n*🔰 Motivo de la inactividad${text ? ': ' + text : ''}*
+  m.reply(`*[ ⚠️ ] El usuario ${conn.getName(m.sender)} se ha desconectado de la partida (AFK) no lo etiqueten*\n\n*🔰 Motivo de la inactividad${text ? ': ' + text : ''}*
 `)
 }
 handler.help = ['afk [alasan]']
