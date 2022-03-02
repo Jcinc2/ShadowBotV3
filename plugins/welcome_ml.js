e107fae2bb494ab7c0bcc0366af8bf84c786548b
Jcinc2 <91334198+Jcinc2@users.noreply.github.com>
2022-03-02 01:45:30 -0500

    Update welcome_ml.js

--- plugins/welcome_ml.js
+++ plugins/welcome_ml.js
@@ -2,12 +2,12 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/baneado.mp3'
-conn.sendFile(m.chat, vn, 'baneado.mp3', null, m, true, {
+let vn = './media/Welcome ml.mp3'
+conn.sendFile(m.chat, vn, 'Welcome ml.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /baneado|Baneado/
+handler.customPrefix = /welcomeml|welcome ml/
 handler.command = new RegExp
 module.exports = handler
