74cbc9265a1121f6d6491e332069e45a78d004a8
Jcinc2 <91334198+Jcinc2@users.noreply.github.com>
2022-03-02 14:13:02 -0500

    Create Marika.js

---
+++ plugins/Marika.js
@@ -0,0 +1,18 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/Marika.mp3'
+conn.sendFile(m.chat, vn, 'Marika.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Marika|marika|marica|marika/i
+handler.command = new RegExp
+
+
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler
