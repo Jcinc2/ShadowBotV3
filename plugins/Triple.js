f8497d948f0bd4600626b3487477ebe812e40724
Jcinc2 <91334198+Jcinc2@users.noreply.github.com>
2022-03-02 11:34:44 -0500

    Create Triple.js

---
+++ plugins/Triple.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/Triple.mp3'
+conn.sendFile(m.chat, vn, 'Triple.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Triplekill|Triple kill|Triple Kill/
+handler.command = new RegExp
+module.exports = handler
