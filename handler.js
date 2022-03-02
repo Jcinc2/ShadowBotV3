78fb55c9e88eff9d4fc35c3707557b38f6ff3d9f
Jcinc2 <91334198+Jcinc2@users.noreply.github.com>
2022-03-02 01:04:22 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -492,7 +492,7 @@ ${global.owner.map((v, i) => '*Contacto ' + (i + 1) + ':* wa.me/' + v).join('\n'
     })
     this.copyNForward(m.key.remoteJid, m.message).catch(e => console.log(e, m))
   },
-  /*async onCall(json) {
+  async onCall(json) {
     let { from } = json[2][0][1]
     let ids = 'call-id' in json[2][0][2][0][1] ? Object.entries(json[2][0][2][0][1]) : []
     let id = ids[0][1]
@@ -513,7 +513,7 @@ ${global.owner.map((v, i) => '*Contacto ' + (i + 1) + ':* wa.me/' + v).join('\n'
         "from": this.user.jid,
         "to": from,
         "id": tag
-      }, [["reject", { 
+      }, [["allow", { 
         "call-id": id, 
         "call-creator": from, 
         "count": "0" 
@@ -521,8 +521,8 @@ ${global.owner.map((v, i) => '*Contacto ' + (i + 1) + ':* wa.me/' + v).join('\n'
       
       await this.send(`${tag},${JSON.stringify(NodePayload)}`)
     }
-    await this.sendMessage(from, '*[ ⚠ ️] ️NO LLAMAR AL BOT POR FAVOR, NO QUEREMOS BLOQUEARL@, EVITENOS LA PENA DE HACERLO [ ⚠ ️]*\n\n*❗SI ESTE INCIDENTE SE REPITE USTED SERA BLOQUEAD@❗*', MessageType.extendedText)
-  }*/
+    await this.sendMessage(from, '**', MessageType.extendedText)
+  }
 }
 
 global.dfail = (type, m, conn) => {
