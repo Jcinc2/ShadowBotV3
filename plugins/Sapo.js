0ded9265b2d8b9024d7d31407387b222b5e3f9bc
Jcinc2 <91334198+Jcinc2@users.noreply.github.com>
2022-03-17 13:16:31 -0500

    Update Sapo.js

--- plugins/Sapo.js
+++ plugins/Sapo.js
@@ -2,13 +2,13 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/Aparta.mp3'
-conn.sendFile(m.chat, vn, 'Aparta.mp3', null, m, true, {
+let vn = './media/Sapo.mp3'
+conn.sendFile(m.chat, vn, 'Sapo.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /Aparta|APARTA|aparta|/i
+handler.customPrefix = /Sapo|SAPO|sapo|/i
 handler.command = new RegExp
 
 
